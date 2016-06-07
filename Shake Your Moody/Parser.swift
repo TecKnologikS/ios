0ed808de55b4e1c89629940f5fae827f401aa535
= <robinpauquet@gmail.com>
2016-06-07 19:37:52 +0200

    none

---
+++ Shake Your Moody/Parser.swift
@@ -0,0 +1,33 @@
+//
+//  Parser.swift
+//  Shake Your Moody
+//
+//  Created by Robin PAUQUET on 07/06/2016.
+//  Copyright © 2016 Robin PAUQUET. All rights reserved.
+//
+
+import Foundation
+
+class Parser {
+    /*func getJSON(urlToRequest: String) -> NSData{
+        return NSData(contentsOfURL: NSURL(string: urlToRequest)!)!
+    }
+    func jsonToData(data:NSData) -> [Product] {
+        var products = [Product]()
+        do {
+            if let JSONObject = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments) as? [[String: AnyObject]] {
+                for p in JSONObject {
+                    var product:Product!
+                    product.about = (p["about"] as? String)!
+                    products.append(product)
+                }
+                // There's our username
+            }
+        } catch {
+            print("error serializing JSON: \(error)")
+        }
+        return products
+    }
+    */
+
+}
--- Shake Your Moody/ViewController.swift
+++ Shake Your Moody/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        //var products:[Product] = jsonToData(getJSON("http://163.172.27.134/api/products"))
         //test
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -20,6 +21,54 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    /*
+    func getJSON(urlToRequest: String) -> NSData{
+        return NSData(contentsOfURL: NSURL(string: urlToRequest)!)!
+    }
+    
+    static func convert(src: NSString) -> NSDictionary {
+        // convert String to NSData
+        let data = src.dataUsingEncoding(NSUTF8StringEncoding)
+        var error: NSError?
+        
+        // convert NSData to 'AnyObject'
+        let anyObj: AnyObject?
+        do {
+            anyObj = try NSJSONSerialization.JSONObjectWithData(data!, options: NSJSONReadingOptions(rawValue: 0))
+        } catch let error1 as NSError {
+            error = error1
+            anyObj = nil
+        }
+        
+        if(error != nil) {
+            // If there is an error parsing JSON, print it to the console
+            print("JSON Error \(error!.localizedDescription)")
+            //self.showError()
+            return NSDictionary()
+        } else {
+            
+            return anyObj as! NSDictionary
+        }
+        
+    }
+    
+    func jsonToData(data:NSData) -> [Product] {
+        var products = [Product]()
+        do {
+            if let JSONObject = try NSJSONSerialization.JSONObjectWithData(data, options: .AllowFragments) as? [[String: AnyObject]] {
+                for p in JSONObject {
+                    var product:Product!
+                    product.about = p["about"]!.variable
+                    products.append(product)
+                }
+                // There's our username
+            }
+        } catch {
+            print("error serializing JSON: \(error)")
+        }
+        return products
+    }*/
+
 
 
 }
