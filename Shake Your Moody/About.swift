f7a16652428bb82acbb022ce018c7ce7a8482819
= <robinpauquet@gmail.com>
2016-06-08 12:27:31 +0200

    extension string & string ressource

---
+++ Shake Your Moody/About.swift
@@ -0,0 +1,19 @@
+//
+//  About.swift
+//  Shake Your Moody
+//
+//  Created by Robin PAUQUET on 08/06/2016.
+//  Copyright © 2016 Robin PAUQUET. All rights reserved.
+//
+
+import Foundation
+
+class About {
+    static let title:String = ""
+    static let aPropos:String = ""
+    static let creator:[String] = ["", "", ""]
+    static let creatorRules:[String] = ["", "", ""]
+    static let introFeature:String = ""
+    static let features:[String] = ""
+    
+}
