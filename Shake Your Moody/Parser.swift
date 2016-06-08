7e130c41e7e2074fe50c523e4f9a8dba4cf1c756
= <robinpauquet@gmail.com>
2016-06-08 10:49:13 +0200

    parser finish

--- Shake Your Moody/Parser.swift
+++ Shake Your Moody/Parser.swift
@@ -35,27 +35,42 @@ class Parser {
     }
     
     func jsonTODATA2(data:NSData) {
-        var products = [Product]()
         do {
             let jsonResult = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers)
             for anItem in jsonResult as! [Dictionary<String, AnyObject>] {
-                var p = Product()
+                let p = Product()
                 p.about = anItem[JSON.ABOUT] as! String
                 p.title = anItem[JSON.TITlE] as! String
                 p.picture = anItem[JSON.PICT] as! String
                 p.thumbnail = anItem[JSON.THUMB] as! String
                 p.id = anItem[JSON.ID] as! String
+                for tag in anItem[JSON.TAGS] as! [String] {
+                    p.tags.append(tag)
+                }
+                let criteres = anItem[JSON.CRITERIA] as! [String : AnyObject]
                 
-                let critere = anItem[JSON.CRITERIA] as! [Dictionary<String, AnyObject>]
-                //let personID = anItem["id"] as! Int
+                let hair = criteres[JSON.HAIR] as! [String:AnyObject]
+                p.hair?.bright = hair[JSON.BRIGHT] as! Bool
+                p.hair?.dark = hair[JSON.DARK] as! Bool
+                
+                let mood = criteres[JSON.MOOD] as! [String:AnyObject]
+                p.mood?.chill = mood[JSON.CHILL] as! Bool
+                p.mood?.party = mood[JSON.PARTY] as! Bool
+                p.mood?.work = mood[JSON.WORK] as! Bool
+                p.mood?.weekend = mood[JSON.WEEKEND] as! Bool
+                
+                let gender = criteres[JSON.GENDER] as! [String:AnyObject]
+                p.gender?.male = gender[JSON.MALE] as! Bool
+                p.gender?.female = gender[JSON.FEMALE] as! Bool
+                
+                let skin = criteres[JSON.SKIN] as! [String:AnyObject]
+                p.skin?.bright = skin[JSON.BRIGHT] as! Bool
+                p.skin?.dark = skin[JSON.DARK] as! Bool
+                
                 products.append(p)
-                // do something with personName and personID
             }
         } catch let error as NSError {
             print(error)
-        }
-        for p in products {
-            print("coucou")
         }
     }
 
